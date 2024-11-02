79689fae8fc534e801d1375c30f8caf7b305c750
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-11-03 01:53:04 +0700

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Components/**/*.{razor,html}",
+    "./Pages/**/*.{razor,html}",
+    "./wwwroot/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
